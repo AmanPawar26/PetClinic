6b4b08c8557c205a2660a2bc637c0558b25eb52f
Aman <amanrpxx@gmail.com>
2025-05-31 20:15:18 +0530

    Logic for fetching(GET) vets with their corresponding speciality

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import ownerRoutes from './routes/ownerRoutes.js';
-import petTypeRoutes from './routes/petTypeRoutes.js'
-import petRoutes from './routes/petRoutes.js'
-import visitRoutes from './routes/visitRoutes.js'
+import petTypeRoutes from './routes/petTypeRoutes.js';
+import petRoutes from './routes/petRoutes.js';
+import visitRoutes from './routes/visitRoutes.js';
+import vetRoutes from './routes/vetRoutes.js';
+
 
 const app = express();
 
@@ -15,8 +17,12 @@ app.use(cors());
 //ROUTES
 app.use('/', ownerRoutes);
 app.use('/', petTypeRoutes);
-app.use('/', petRoutes)
+app.use('/', petRoutes);
 app.use('/', visitRoutes);
+app.use('/', vetRoutes);
+
+
+
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
