fa71a64952fb3aabbc3d9b02622c797695f3f96c
Aman <amanrpxx@gmail.com>
2025-06-02 19:54:07 +0530

    Swagger documentation for vetRoutes.js

--- backend/routes/vetRoutes.js
+++ backend/routes/vetRoutes.js
@@ -3,6 +3,39 @@ import * as vetController from "../controllers/vetController.js";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Vets
+ *   description: Veterinarian management
+ */
+
+/**
+ * @swagger
+ * /vets:
+ *   get:
+ *     summary: Get all veterinarians
+ *     tags: [Vets]
+ *     responses:
+ *       200:
+ *         description: A list of veterinarians
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     example: Dr. Jane Smith
+ *                   specialty:
+ *                     type: string
+ *                     example: Surgery
+ */
 router.get('/vets', vetController.getAllVets);
 
 export default router;
